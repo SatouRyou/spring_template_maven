ef08d6d43089e627f7aa21ca9a97421ed14f8798
SatouRyou <satouryou1205@gmail.com>
2016-10-01 21:28:02 +0900

    パッケージ構成

---
+++ src/main/java/com/springTemplateMaven/SpringTemplateMavenApplication.java
@@ -0,0 +1,12 @@
+package com.springTemplateMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateMavenApplication.class, args);
+	}
+}
