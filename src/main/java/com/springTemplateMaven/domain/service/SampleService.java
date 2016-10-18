c63989190d4e38b096d4e5bbc7a685af1badb976
SatouRyou <satouryou1205@gmail.com>
2016-10-19 00:09:59 +0900

    トランザクションの設定

--- src/main/java/com/springTemplateMaven/domain/service/SampleService.java
+++ src/main/java/com/springTemplateMaven/domain/service/SampleService.java
@@ -6,6 +6,7 @@ import com.springTemplateMaven.domain.vo.EmpVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,19 +20,31 @@ public class SampleService {
     @Autowired
     private SampleTrust sampleTrust;
 
+    // トランザクション設定アノテーション
+    // noRollbackForは指定したExceptionでロールバックを発生させない
+    // サブクラスも対象なので、全てのExceptionはロールバックされない
+    @Transactional(noRollbackFor = Exception.class)
     public List<EmpDto> getAll() {
+
         return this.sampleTrust.getAll();
     }
 
+    // トランザクション設定アノテーション
+    // rollbackForは指定したExceptionでロールバックをおこなう
+    // サブクラスも対象なので全てのExceptionクラスがロールバック対象
+    @Transactional(rollbackFor = Exception.class)
     public EmpDto get( int id ) {
+
         return this.sampleTrust.get( id );
     }
 
     public void put( EmpVo empVo ) {
+
         this.sampleTrust.put( empVo );
     }
 
     public void delete( int id ) {
+
         this.sampleTrust.delete( id );
     }
 }
