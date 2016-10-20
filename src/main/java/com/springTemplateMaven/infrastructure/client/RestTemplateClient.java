0ee7c323ebf0b73f5ae48498615d996eb6f53af1
SatouRyou <satouryou1205@gmail.com>
2016-10-21 01:26:03 +0900

    基本的なRestTemplateの実装

--- src/main/java/com/springTemplateMaven/infrastructure/client/RestTemplateClient.java
+++ src/main/java/com/springTemplateMaven/infrastructure/client/RestTemplateClient.java
@@ -1,16 +1,36 @@
 package com.springTemplateMaven.infrastructure.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 /**
  * Created by teradashoutarou on 2016/10/20.
  */
 public class RestTemplateClient implements Client {
 
-    private RestTemplate restTemplate;
+    @Autowired
+    // RestTemplateを使用する際、インターフェースである
+    // RestOperationsを使用した方が依存性を下げられる
+    private RestOperations restOperations;
 
     public RestTemplateClient() {
-        restTemplate = new RestTemplate();
+        // リクエストヘッダーを設定する際は、
+        // RequestEntityのビルダーパターンで実装するのがベスト
+        RequestEntity<String> requestEntity
+                = RequestEntity
+                .post(URI.create("URI") )
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept()
+                .header("")
+                .body("");
+
+        // 通信実行
+        restOperations.exchange( requestEntity, String.class );
     }
 
 }
